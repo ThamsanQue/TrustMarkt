c5e7f33eda22b245887ba5c6a310163c900f1445
JunadeMorningstar <junade.dev@gmail.com>
2024-03-02 23:29:27 +0200

    set up deepface

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { env } from "./src/env.js";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +7,18 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5134/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
+};
 
 export default config;
